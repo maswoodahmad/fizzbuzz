b3ed4f8d7f1d1d9ee653a316df1482ab4ee75d18
Maswood Ahmad <55253162+maswoodahmad@users.noreply.github.com>
2023-04-13 17:28:51 +0530

    modified:   fizzBuzz.js

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -1,17 +1,19 @@
-function fizzBuzz(phone){
-    for( let i =1; i<=10; i++)
-    {
-        if (i % 4 === 0 && i % 5 === 0) {
-            console.log("FizzBuzz");
-          } else if (i % 4 === 0) {
-            console.log("Fizz");
-          } else if (i % 5 === 0) {
-            console.log("Buzz");
-          } else {
-            console.log(i);
-          }
-        }
+function fizzBuzz(phone) {
+    let sum = 0;
+    for (let digit of phone) {
+      sum += parseInt(digit);
     }
-    let  num=9936675027;
-    fizzBuzz(num);
+    for (let i = 1; i <= sum; i++) {
+      if (i % 4 === 0 && i % 5 === 0) {
+        console.log("FizzBuzz");
+      } else if (i % 4 === 0) {
+        console.log("Fizz");
+      } else if (i % 5 === 0) {
+        console.log("Buzz");
+      } else {
+        console.log(i);
+      }
+    }
+  }
+  fizzBuzz("1234567890");
 
